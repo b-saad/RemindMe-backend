f7053990c3b0cd97a6f72c2b6f49eb0a9e8aa916
Bilal Saad <bilalsaad923@gmail.com>
2019-12-27 23:40:53 -0500

    update dummy post request

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 8000;
 
-app.post('/remind', (req, res) => 
-    res.send('reminded')
-);
+app.use(bodyParser.json());
+
+app.post('/api/remind', (req, res) => {
+    const { phoneNumber, date, message } = req.body;
+    res.status(200).send(`The message \'${message}\' will be sent to ${phoneNumber} on ${date}`);
+});
 
 app.listen(PORT, () => 
     console.log(`Listening on port ${PORT}`)
