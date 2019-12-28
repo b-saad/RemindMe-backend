0134609b74ce22b8f9413ea5645606f340319ee2
Bilal Saad <bilalsaad923@gmail.com>
2019-12-27 22:52:57 -0500

    add dummy response for post

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
 
 const app = express();
-const PORT = 8000
+const PORT = 8000;
+
+app.post('/remind', (req, res) => 
+    res.send('reminded')
+);
 
 app.listen(PORT, () => 
     console.log(`Listening on port ${PORT}`)
