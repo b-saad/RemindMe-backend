88d148903213542eab5da2656c21df9212228b0b
Bilal Saad <bilalsaad923@gmail.com>
2019-12-29 17:24:27 -0500

    add cron scheduler and db read and write queries

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import { db_host, db_user, db_password, db_name } from './config.json';
 
 const mysql = require('mysql');
+const CronJob = require('cron').CronJob;
 const app = express();
 const PORT = 8000;
 
@@ -13,30 +14,82 @@ const db = mysql.createConnection ({
     database: db_name
 });
 
-
-
 app.use(bodyParser.json());
 
 // TEST ENDPOINT
 app.get('/api/reminders', (req, res) => {
-    db.connect((err) => {
-        if (err) throw err;
-        console.log('Connected!');
-    });
-
-    const query = "SELECT * FROM reminder";
+    // db.connect((err) => {
+    //     if (err) throw err;
+    //     console.log('Connected!');
+    // });
+    const query = 'SELECT * FROM reminder;'
     db.query(query, (err, result) => {
-        // console.log(result);
         res.status(200).send(`Reminders:\n \'${JSON.stringify(result)}\'`);
     });
 
-    db.end();
+    // db.end();
+});
+
+app.post('/api/cron', (req, res) => {
+    const { date } = req.body;
+    new CronJob(
+        new Date(date),
+        function() {
+            console.log('cron job fired');
+            this.stop();
+        },
+        null,
+        true
+    );
+    res.status(200).send();
 });
 
 app.post('/api/remind', (req, res) => {
-    const { phoneNumber, date, message } = req.body;
-    res.status(200).send(`The message \'${message}\' will be sent to ${phoneNumber} on ${date}`);
+    const { phoneNumber, message, date } = req.body;
+    const insertQuery = `INSERT INTO reminder (phone_number, message) VALUES ("${phoneNumber}", "${message}");`;
+    const selectLastIdQuery = 'SELECT LAST_INSERT_ID();'
+    db.query(insertQuery, (err, result) => {
+        if (err) {
+            res.status(500).send(`An error ocurred: ${err}`);
+            return;
+        }
+        db.query(selectLastIdQuery, (err, result) => {
+            if (err) {
+                res.status(500).send(`An error ocurred: ${err}`);
+                return;
+            }
+            sendReminder(result[0]['LAST_INSERT_ID()'], date);
+        });
+        res.status(200).send(`inserted`);
+    });
 });
+
+/*
+* Sends a reminder with `reminder_id` at provided `date`
+* @param reminder_id: int
+* @param date: String - UTC 'YYYY-MM-DDThh:mm:ssZ'
+*/
+function sendReminder(reminder_id, date) {
+
+    new CronJob(
+        new Date(date),
+        function() {
+            const selectReminderQuery = `SELECT * FROM reminder WHERE reminder_id = ${reminder_id}`;
+            db.query(selectReminderQuery, (err, result) => {
+                if (err) {
+                    console.log(`An error ocurred when retrieving the reminder: ${err}`);
+                    return;
+                }
+                const { phone_number, message } = result[0];
+                console.log(`${phone_number} - ${message}`);
+            
+                });
+            this.stop();
+        },
+        null,
+        true
+    );
+}
 
 app.listen(PORT, () => 
     console.log(`Listening on port ${PORT}`)
