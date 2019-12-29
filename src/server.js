04e0fad6949445d058f4d02baab87411721865ec
Bilal Saad <bilalsaad923@gmail.com>
2019-12-28 23:19:27 -0500

    add db, and test endpoint

--- src/server.js
+++ src/server.js
@@ -1,10 +1,37 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { db_host, db_user, db_password, db_name } from './config.json';
 
+const mysql = require('mysql');
 const app = express();
 const PORT = 8000;
 
+const db = mysql.createConnection ({
+    host: db_host,
+    user: db_user,
+    password: db_password,
+    database: db_name
+});
+
+
+
 app.use(bodyParser.json());
+
+// TEST ENDPOINT
+app.get('/api/reminders', (req, res) => {
+    db.connect((err) => {
+        if (err) throw err;
+        console.log('Connected!');
+    });
+
+    const query = "SELECT * FROM reminder";
+    db.query(query, (err, result) => {
+        // console.log(result);
+        res.status(200).send(`Reminders:\n \'${JSON.stringify(result)}\'`);
+    });
+
+    db.end();
+});
 
 app.post('/api/remind', (req, res) => {
     const { phoneNumber, date, message } = req.body;
@@ -14,3 +41,4 @@ app.post('/api/remind', (req, res) => {
 app.listen(PORT, () => 
     console.log(`Listening on port ${PORT}`)
 );
+
