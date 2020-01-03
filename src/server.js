917997ea389e055fdbbe0eaaa271d18f1cbfd537
Bilal Saad <bilalsaad923@gmail.com>
2020-01-02 20:45:17 -0500

    add https support and fix cors errors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,37 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import db from './db' ;
+import { path_to_ssl_cert, path_to_ssl_private_key, path_to_ssl_chain } from './config.json';
 import { createReminder } from './reminder';
 
-const CronJob = require('cron').CronJob;
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
 const app = express();
-const PORT = 8000;
+const HTTP_PORT = 8000;
+const HTTPS_PORT = 9000;
+
+// To fix CORS errors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+/* 
+* Certificates from letsencrypt - file paths should be of format
+* "/etc/letsencrypt/live/{domain}/{file}"
+*/
+const privateKey = fs.readFileSync(path_to_ssl_private_key, 'utf8');
+const certificate = fs.readFileSync(path_to_ssl_cert, 'utf8');
+const ca = fs.readFileSync(path_to_ssl_chain, 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 app.use(bodyParser.json());
 
@@ -15,9 +41,10 @@ app.use(bodyParser.json());
 * message - String
 * date - String - UTC 'YYYY-MM-DDThh:mm:ssZ'
 */
-app.post('/api/remind', (req, res) => {
+app.post('/remind', (req, res) => {
+    console.log("request received");
     const { phoneNumber, message, date } = req.body;
-    createReminder(phoneNumber, message, date, (err) => { 
+    createReminder(phoneNumber, message, date, (err) => {
         if (err) {
             res.status(500).send(`An error ocurred: ${err}`)
         } else {
@@ -26,7 +53,14 @@ app.post('/api/remind', (req, res) => {
     });
 });
 
-app.listen(PORT, () => 
-    console.log(`Listening on port ${PORT}`)
-);
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
+httpServer.listen(HTTP_PORT, () => {
+	console.log(`HTTP Server running on port ${HTTP_PORT}`);
+});
+
+httpsServer.listen(HTTPS_PORT, () => {
+	console.log(`HTTPS Server running on port ${HTTPS_PORT}`);
+});
